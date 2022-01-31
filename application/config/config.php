7117fe034010c35e6a7c11746a46dcad6a07ca04
dhonstudio <doon13@gmail.com>
2022-01-31 12:18:12 +0700

    Set autoload, config, and routes; create Home

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = ENVIRONMENT == 'development' ? 'http://localhost/ci_dashboard/' : 'https://dhonstudio.com/ci/dashboard/';
 
 /*
 |--------------------------------------------------------------------------
---
+++ application/controllers/Home.php
@@ -0,0 +1,46 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+date_default_timezone_set('Asia/Jakarta');
+
+class Home extends CI_Controller {
+    
+    public function __construct()
+	{
+		parent::__construct();
+
+        require_once __DIR__ . '/../../assets/ci_helpers/style_helper.php';
+        require_once __DIR__ . '/../../assets/ci_libraries/DhonAPI.php';
+        $this->dhonapi = new DhonAPI;
+
+        $this->dhonapi->api_url['development'] = 'http://localhost/ci_api/';
+        $this->dhonapi->api_url['production'] = 'https://dhonstudio.com/ci/api/';
+        $this->dhonapi->username = 'admin';
+        $this->dhonapi->password = 'admin';
+
+        $this->lang = 'en';
+	}
+
+	public function index()
+	{
+        $data = [
+            'title'         => 'SB Admin - Dashboard',
+            'css'           => [
+                $this->css['sb-admin'],
+                $this->css['fontawesome5'],
+            ],
+            'body_class'    => 'sb-nav-fixed',
+            'js'            => [
+                $this->js['bootstrap-bundle5'],
+                $this->js['sb-admin'],
+            ],
+        ];
+
+        $this->load->view('ci_templates/header', $data);
+        $this->load->view('home');
+        $this->load->view('copyright');
+        $this->load->view('ci_templates/toast');
+        $this->load->view('ci_templates/footer');
+        $this->load->view('ci_templates/end');
+    }
+}
